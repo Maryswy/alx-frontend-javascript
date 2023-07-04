e1b0ac6cc948de3fe899171f7ab1c5affca08ae7
Maryswy <marysowa54@gmail.com>
2023-07-05 00:06:44 +0300

    a function getStudentsByLocation that returns an array of objects who are located in a specific city (Filter)

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,13 @@
-function getStudentsByLocation(students, city) {
-  return students.filter(student => student.location === city);
-}
+// This function takes two arguments: 'students' (an array) and 'city' (a string).
+export default function getStudentsByLocation(students, city) {
+  // The code checks if the 'students' argument is an array by comparing its prototype to the Array.prototype.
+  if (Object.getPrototypeOf(students) === Array.prototype) {
+    // If 'students' is an array, the filter() method is used to create a new array with elements that match the condition.
+    // The condition in this case checks if the 'location' property of each student object is equal to the provided 'city' value.
+    // The filter() method returns a new array containing only the student objects that satisfy the condition.
+    return students.filter((items) => items.location === city);
+  }
 
-module.exports = getStudentsByLocation;
+  // If 'students' is not an array, the code returns an empty array using 'return []'.
+  return [];
+}

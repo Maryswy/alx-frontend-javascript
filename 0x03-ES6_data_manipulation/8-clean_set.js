8d5e49cf88d0c3a1c188eb5516efce9cf1fa7d9c
Maryswy <marysowa54@gmail.com>
2023-07-05 00:25:27 +0300

    Creating a function named cleanSet that returns a string of all the set values that start with a specific string (startString).It accepts two arguments: a set (Set) and a startString (String)

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,23 @@
-function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
-  return filteredValues.join("-");
-}
+// This function takes two arguments: 'set' (a Set) and 'startString' (a string).
+export default function cleanSet(set, startString) {
+  // Checks if 'startString' exists and is of type 'string'.
+  if (startString && typeof startString === 'string') {
+    // Creates an empty array to store the filtered and modified strings.
+    const strings = [];
 
-module.exports = cleanSet;
+    // Iterates over each item in the 'set'.
+    for (const item of set) {
+      // Checks if the current 'item' starts with the 'startString'.
+      if (item.startsWith(startString)) {
+        // If it does, removes the 'startString' from the 'item' and adds the modified string to the 'strings' array.
+        strings.push(item.slice(startString.length));
+      }
+    }
+
+    // Joins the modified strings in the 'strings' array using a hyphen (-) as the separator.
+    return strings.join('-');
+  }
+
+  // If 'startString' is not provided or is not of type 'string', it returns an empty string.
+  return '';
+}

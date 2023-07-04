a0ef15d7cebda0c5f5a219cc22373c9eb4933be7
Maryswy <marysowa54@gmail.com>
2023-07-05 00:29:25 +0300

    Creating a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1 (More map data structure)

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,18 +1,20 @@
-function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error("Cannot process");
+// This function takes one argument: 'groceries' (a Map object).
+export default function updateUniqueItems(groceries) {
+  // Checks if the prototype of 'groceries' is not equal to Map.prototype.
+  if (Object.getPrototypeOf(groceries) !== Map.prototype) {
+    // If 'groceries' is not an instance of the Map class, it throws a TypeError.
+    throw TypeError('Cannot process');
   }
 
-  const updatedMap = new Map();
-  map.forEach((quantity, item) => {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
+  // Iterates over each key-value pair in the 'groceries' Map using the forEach() method.
+  groceries.forEach((val, key) => {
+    // Checks if the value (val) is equal to 1.
+    if (val === 1) {
+      // If the value is 1, it updates the value to 100 for the corresponding key in the 'groceries' Map using the set() method.
+      groceries.set(key, 100);
     }
   });
 
-  return updatedMap;
+  // Returns the modified 'groceries' Map.
+  return groceries;
 }
-
-module.exports = updateUniqueItems;

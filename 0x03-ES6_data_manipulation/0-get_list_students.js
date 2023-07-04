2945d90045b35d3b2dec2d2591023b9ee7991e8e
Maryswy <marysowa54@gmail.com>
2023-07-04 23:43:06 +0300

    Creating a function named getListStudents that returns an array of objects (Basic list of objects)

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,6 @@
+// Defines a function that returns a list of students
 function getListStudents() {
+  // Returns an array of student objects
   return [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
@@ -6,4 +8,6 @@ function getListStudents() {
   ];
 }
 
-module.exports = getListStudents;
+// Exports the `getListStudents` function as the default export
+export default getListStudents;
+

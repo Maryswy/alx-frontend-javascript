60aa3c078a287ca9b09b698bad80a0081a56e4f9
Maryswy <marysowa54@gmail.com>
2023-07-05 00:32:57 +0300

    Exporting a const instance of WeakMap and name it weakMap (Weak link data structure)

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,17 +1,20 @@
-const weakMap = new WeakMap();
+// Creates a new WeakMap object called 'weakMap'.
+export const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-  const { protocol, name } = endpoint;
-  const endpointKey = `${protocol}:${name}`;
+// Defines a function called 'queryAPI' that takes an 'endpoint' as an argument.
+export function queryAPI(endpoint) {
+  // Checks if the 'endpoint' key is not present in the 'weakMap'.
+  if (!weakMap.has(endpoint)) {
+    // If the 'endpoint' key is not present, it sets the 'endpoint' key in the 'weakMap' with an initial value of 0.
+    weakMap.set(endpoint, 0);
+  }
 
-  let queryCount = weakMap.get(endpointKey) || 0;
-  queryCount++;
+  // Retrieves the current value associated with the 'endpoint' key from the 'weakMap' and increments it by 1.
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 
-  weakMap.set(endpointKey, queryCount);
-
-  if (queryCount >= 5) {
-    throw new Error(`Endpoint load is high: ${endpointKey}`);
+  // Checks if the value associated with the 'endpoint' key in the 'weakMap' is equal to or exceeds 5.
+  if (weakMap.get(endpoint) >= 5) {
+    // If the value is equal to or exceeds 5, an error is thrown indicating that the load on the 'endpoint' is high.
+    throw Error('Endpoint load is high');
   }
 }
-
-module.exports = { weakMap, queryAPI };

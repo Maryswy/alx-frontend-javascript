850f7aa84d58a148a979fea337f0c1c1e23a44b0
Maryswy <marysowa54@gmail.com>
2023-07-05 00:10:40 +0300

    Creating a function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position (Typed Arrays)

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,23 @@
-function createInt8TypedArray(length, position, value) {
+// This function takes three arguments: 'length' (number), 'position' (number), and 'value' (number).
+export default function createInt8TypedArray(length, position, value) {
+  // Creates a new ArrayBuffer object with the specified 'length'.
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  
+  // Creates a DataView object using the created 'buffer'.
+  // The DataView is used to read and write data to the underlying ArrayBuffer.
+  // The second and third arguments of the DataView constructor define the byte offset and byte length respectively.
+  const view = new DataView(buffer, 0, length);
 
-  if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+  // Checks if the 'position' argument is outside the range of the array.
+  if (position >= length) {
+    // If the position is outside the range, an error is thrown using 'throw Error()'.
+    throw Error('Position outside range');
   }
 
-  int8Array[position] = value;
+  // Sets the value at the specified 'position' in the Int8 typed array.
+  // The 'setInt8' method of the DataView is used to write a signed 8-bit integer value at the specified byte 'position'.
+  view.setInt8(position, value);
 
-  return buffer;
+  // Returns the created DataView object.
+  return view;
 }
-
-module.exports = createInt8TypedArray;

8904d5b0fcfc5c78b1e30d50fd558ae2ac2ad2a4
Maryswy <marysowa54@gmail.com>
2023-07-05 00:03:14 +0300

    a function getListStudentIds that returns an array of ids from a list of object (More mapping)

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,18 @@
-// Defines a function named getListStudentIds
-export default function getListStudentIds(students) {
-  // Checks if the argument is an array before using the map method
-  if (Array.isArray(students)) {
-    // Maps over the students array and extracts the 'id' property from each object
-    return students.map((items) => items.id);
+// This function takes an array 'arr' as input and returns an array containing the IDs of students from the input array.
+function getListStudentIds(arr) {
+  // First, the code checks if the input 'arr' is not an array using the Array.isArray() method.
+  if (!Array.isArray(arr)) {
+    // If 'arr' is not an array, it returns an empty array using 'return []'.
+    return [];
   }
-  // If the argument is not an array, returns an empty array
-  return [];
+
+  // If 'arr' is an array, the map() method is used to iterate over each element in the array.
+  // The map() method transforms each element in the array to its corresponding 'id' property (assuming each element is an object with an 'id' property).
+  // The arrow function 'student => student.id' is used to extract the 'id' property of each student object in the 'arr' array.
+  // The map() method returns a new array containing only the 'id' properties of the student objects.
+
+  return arr.map(student => student.id);
 }
 
+// Exporting the function to be used in other parts of the application.
+export default getListStudentIds;

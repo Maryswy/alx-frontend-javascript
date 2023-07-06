d58ea8a1a4c117ef044079b1ab47f458328b774e
Maryswy <marysowa54@gmail.com>
2023-07-06 03:56:16 +0300

    Creating an an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,4 +1,4 @@
-// Define the Student interface
+// Define the interface for a Student
 interface Student {
   firstName: string;
   lastName: string;
@@ -22,29 +22,48 @@ const student2: Student = {
 };
 
 // Create an array containing the two student objects
-const studentsList: Student[] = [student1, student2];
+const studentsList: Array<Student> = [student1, student2];
 
-// Get the table element from the HTML document
-const table = document.createElement("table");
+// Get the body element from the HTML document
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
 
-// Create a table row for each student and append it to the table
+// Create a table element
+const table: HTMLTableElement = document.createElement("table");
+
+// Create table sections for the table
+const thead: HTMLTableSectionElement = document.createElement("thead");
+const tbody: HTMLTableSectionElement = document.createElement("tbody");
+
+// Create a row for the table header
+const rowHead: HTMLTableRowElement = thead.insertRow(0);
+
+// Create cells for the table header
+const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
+const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+
+// Set the content of the table header cells
+cell1Head.innerHTML = "firstName";
+cell2Head.innerHTML = "location";
+
+// Append the table header to the table
+table.append(thead);
+
+// Iterate over each student in the studentsList array
 studentsList.forEach((student) => {
-  const row = document.createElement("tr");
-  
-  // Create table cells for the first name and location
-  const firstNameCell = document.createElement("td");
-  firstNameCell.textContent = student.firstName;
-  
-  const locationCell = document.createElement("td");
-  locationCell.textContent = student.location;
-  
-  // Append the cells to the row
-  row.appendChild(firstNameCell);
-  row.appendChild(locationCell);
-  
-  // Append the row to the table
-  table.appendChild(row);
+  // Create a row for each student
+  const row: HTMLTableRowElement = tbody.insertRow(0);
+
+  // Create cells for the first name and location
+  const cell1: HTMLTableCellElement = row.insertCell(0);
+  const cell2: HTMLTableCellElement = row.insertCell(1);
+
+  // Set the content of the cells using student data
+  cell1.innerHTML = student.firstName;
+  cell2.innerHTML = student.location;
 });
 
-// Append the table to the document body
-document.body.appendChild(table);
+// Append the table body to the table
+table.append(tbody);
+
+// Append the table to the body element of the document
+body.append(table);
